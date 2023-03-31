6f50da6b677fc406a1653aed360c01a742aa2c6f
oiiboii <omer@tactive.xyz>
2023-03-31 17:45:53 -0400

    add basic UI design

--- Pitch-Perfect-iOS13/Controllers/RecordingViewController.swift
+++ Pitch-Perfect-iOS13/Controllers/RecordingViewController.swift
@@ -15,17 +15,20 @@ class RecordingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopButton.isEnabled = false
     }
 
 
     @IBAction func recordButtonTapped(_ sender: UIButton) {
+        recordButton.isEnabled = false
+        stopButton.isEnabled = true
         recordingStatusLabel.text = "Recording in progress..."
         print("recTapped")
-
+        
     }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        stopButton.isEnabled = false
         print("stopTapped")
 
     }
