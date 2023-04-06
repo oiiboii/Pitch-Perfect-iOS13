c9bce6abc7105929d9cfd7a9c829cff8ed23be56
oiiboii <omer@tactive.xyz>
2023-04-06 09:46:24 -0400

    layout for second view cotroller ready. scales correctly and stays in safe area.

--- Pitch-Perfect-iOS13/Controllers/ModulatorViewController.swift
+++ Pitch-Perfect-iOS13/Controllers/ModulatorViewController.swift
@@ -13,15 +13,30 @@ class ModulatorViewController: UIViewController {
     @IBOutlet weak var ecoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var fastButton: UIButton!
-    
     @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     var recordedAudioURL: URL!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setButtonSize()
 
+        highPitchButton.contentMode = .center
+        highPitchButton.imageView?.contentMode = .scaleAspectFit
         // Do any additional setup after loading the view.
     }
     
+    // looping through each button to set it's size and alignment so the images won't stretch
+    func setButtonSize(){
+        let buttons = [highPitchButton, lowPitchButton, ecoButton, reverbButton, fastButton, slowButton, stopButton]
+        
+        for button in buttons{
+            if let button{
+                button.contentMode = .center
+                button.imageView?.contentMode = .scaleAspectFit
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
